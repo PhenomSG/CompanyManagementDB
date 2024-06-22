c3ed0c6dae69ab2201f9223efd4c1b859dc5b262
Sahaj <sahajguptarocks@gmail.com>
2024-06-22 23:14:38 +0530

    frames

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import tkinter as tk
-from tkinter import messagebox
-from tkinter import ttk
+#from tkinter import messagebox
+#from tkinter import ttk
+# import all idk
+from tkinter import *
 
+# functionalities
 from main import *
+
+# making frames
